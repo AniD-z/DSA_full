d4974b5fc83c1beaf769b277897dbf1ce56f6f88
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2024-01-06 18:31:11 +0530

    remove register button

--- src/components/Events_landing/Events_landing.jsx
+++ src/components/Events_landing/Events_landing.jsx
@@ -17,7 +17,7 @@ export default function Events_landing() {
                   On the 17th of August, 2017, someone has stolen the locker keys of another member in the gym lounge accessible to only those with a silver membership. From the CCTV footage, the culprit had never been seen before or after this date, can you find the culprit?
                 </p>
                 <a href="/input" class="read-more">
-                  Register-now{" "}
+                  {/* Register-now{" "}
                   <span class="sr-only">about this is some title</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +30,7 @@ export default function Events_landing() {
                       d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
                       clip-rule="evenodd"
                     />
-                  </svg>
+                  </svg> */}
                 </a>
               </div>
             </div>
