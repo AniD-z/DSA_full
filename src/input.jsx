bc3cebb074f9bfd4ea63ec2ba89ec179a4939f78
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-01-13 00:07:32 +0530

    Update input.jsx

--- src/input.jsx
+++ src/input.jsx
@@ -23,6 +23,8 @@ function Input() {
     useEffect(() => {
         document.body.style.backgroundColor = "#fff"
         document.body.style.paddingLeft = "14rem";
+        document.body.style.paddingRight = "14rem";
+
         const queryParams = new URLSearchParams(location.search);
         const parameterString = queryParams.get('param');
         if (parameterString) {
