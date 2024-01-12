1caf81659d4cbf9bacb830507dcc5496bcb8b2a7
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-01-12 12:44:01 +0530

    Update input.jsx
    helmet

--- src/input.jsx
+++ src/input.jsx
@@ -77,7 +77,10 @@ function Input() {
     }
 
     return (
-        <div className="center">
+        <div class="sm:mx-auto md:mx-auto lg:mx-0 xl:mx-0">
+            <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
             {/* <h1>Level {passedData.level}</h1>
             <p>{passedData.prob}</p> */}
             <CodeMirror
