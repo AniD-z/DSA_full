13d28ea7dd074450a05fb04ac195bc61c8d94da2
AniD-z <137194673+AniD-z@users.noreply.github.com>
2024-05-09 10:16:16 +0530

    npm update

--- src/components/Eventspage/Eventspage.jsx
+++ src/components/Eventspage/Eventspage.jsx
@@ -5,7 +5,7 @@ export default function Eventspage() {
   return (
     <section className="eventspage ot-red">
       <div className="event-title-box ">
-        <div className="ff-tc event-title title">Levels</div>s
+      <div className="ff-tc event-title title">Levels</div>
       </div>
       <div className="event-list ot-blue"></div>
     </section>
